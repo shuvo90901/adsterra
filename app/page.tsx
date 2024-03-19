6f6af6f4f7cc89d02fdd38cb0d513a50f38d713b
shuvo90901 <shuvo90901@gmail.com>
2024-03-19 09:59:50 +0600

    ads target

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,18 @@ export default function Home() {
   return (
     <main>
       <div className="">
-        
+      {/* <!-- AdsTargets - Ad Display Code --> */}
+<div id="adm-container-25992"></div><script data-cfasync="false" async type="text/javascript" src="//adstargets.com/myAdstargets/display/items.php?25992&14107&728&90&4&0&1"></script>
+{/* <!-- AdsTargets - Ad Display Code --> */}
         <div className="">Testing Testing Testing Testing Testing</div>
         <div className="">Testing app Testing app</div>
-        <ImageUploader/>
+        {/* <ImageUploader/> */}
         <div className="">Testing app Testing app</div>
         <div className="">Testing app Testing app</div>
         <div className="">Testing app Testing app</div>
         <div className="">Testing app Testing app</div>
         {/* <Banner /> */}
-        <QueryPage/>
+        {/* <QueryPage/> */}
         <div className="">Testing app Testing app</div>
         <div className="">Testing app Testing app</div>
         <div className="">Testing app Testing app</div>
